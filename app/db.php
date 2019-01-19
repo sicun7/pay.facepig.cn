bd6384b9b4a0341da7997e99b010c1b6720b212f
ping.zhang <ping@pingzhangdeMacBook-Pro.local>
2019-01-19 15:19:10 +0800

    db.php

---
+++ app/db.php
@@ -0,0 +1,11 @@
+<?php
+    return [
+    'server'=>'118.25.3.251',
+    'port'=>'3306',
+    'user'=>'facepig_test',
+    'pass'=>'facepig_test',
+    'name'=>'facepig_test',
+    'prefix'=>'ys_',
+    'driver'=>'pdo',
+    'debug'=>true,
+];
