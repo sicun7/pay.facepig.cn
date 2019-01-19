c0aa672747c8340ae9db59bac0efb9ed86a9d04c
ping.zhang <ping@pingzhangdeMacBook-Pro.local>
2019-01-19 14:49:52 +0800

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+
+### 程序使用要求？
+* php >= 5.6
+* mysql >= 5.2
+* win or linux
+
+
+后台入口：/ysmd
---
+++ view/default/footer.php
@@ -0,0 +1,29 @@
+
+
+
+<footer data-am-widget="footer"
+        class="am-footer am-footer-default"
+        data-am-footer="{  }">
+    <div class="am-footer-switch">
+        <a id="godesktop" data-rel="desktop" class="am-footer-desktop" href="http://www.facepig.cn" target="_blank">
+            脸猪返利机器人官网
+        </a>
+    </div>
+    <div class="am-footer-miscs ">
+        <p> © Copyright 2018 脸猪返利机器人 版权所有</p>
+        <p><?php echo $this->config['icpcode']?></p>
+    </div>
+</footer>
+
+
+
+
+</body>
+
+</html>
+<script src="/static/default/assets/js/jquery.min.js"></script>
+<script src="/static/default/assets/js/amazeui.min.js"></script>
+<script src="/static/common/layer/layer.js"></script>
+<script src="/static/default/assets/js/app.js"></script>
+<script src="/static/default/js/app.js"></script>
+<?php echo $this->config['stacode']?>
