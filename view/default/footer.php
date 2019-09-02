92ac5444b6f342f481f59396a0ff80671b342898
sicun <if.zhang@foxmail.com>
2019-09-02 19:18:20 +0800

    Update footer.php

--- view/default/footer.php
+++ view/default/footer.php
@@ -5,13 +5,13 @@
         class="am-footer am-footer-default"
         data-am-footer="{  }">
     <div class="am-footer-switch">
-        <a id="godesktop" data-rel="desktop" class="am-footer-desktop" href="http://www.facepig.cn" target="_blank">
-            脸猪机器人官网
+        <a id="godesktop" data-rel="desktop" class="am-footer-desktop" href="http://www.juzicat.com" target="_blank">
+            桔子猫返利机器人官网
         </a>
     </div>
     <div class="am-footer-miscs ">
-        <p> © Copyright 2018 脸猪机器人 版权所有</p>
-        <p><?php echo $this->config['icpcode']?></p>
+        <p> © Copyright 2019 桔子猫返利机器人 版权所有</p>
+        <p>鄂ICP备19013458号</p>
     </div>
 </footer>
 
